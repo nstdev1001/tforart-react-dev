50ea1bf89e1a25eb663d66f8be9c2d2d6e1a4278
nstdev1001 <sontung2000ct@gmail.com>
2025-05-26 00:32:19 +0700

    feat: implement AdminBar component in Navbar for improved admin user experience; streamline admin display logic

---
+++ src/components/Admin/AdminBar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+interface AdminBarProps {
+  user: { email: string | null } | null;
+  checkIsLogin: boolean;
+  handleSignOut: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
+}
+
+const AdminBar: React.FC<AdminBarProps> = ({
+  user,
+  checkIsLogin,
+  handleSignOut,
+}) => {
+  if (!checkIsLogin) return null;
+
+  return (
+    <div
+      className={`fixed bottom-3 left-3 w-[300px] h-[90px] bg-amber-300 z-10 rounded-md`}
+    >
+      {user ? (
+        <div className="relative flex flex-col items-center top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]">
+          <p className="text-black">
+            Hi <span className="font-bold">{user.email}</span>!
+          </p>
+          <p className="text-black">Bạn đang ở chế độ admin.</p>
+          <a
+            href="#"
+            onClick={handleSignOut}
+            className="underline text-red-700"
+          >
+            Đăng xuất
+          </a>
+        </div>
+      ) : (
+        <p>Đang tải...</p>
+      )}
+    </div>
+  );
+};
+
+export default AdminBar;
